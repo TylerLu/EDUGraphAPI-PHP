4eb952bad478f438858c2e3e32e2e74c37de8427
hearticy888 <hearticy@126.com>
2018-05-04 15:38:29 +0800

    Can get accesstoken

--- DataSync/MSGraphHelper.php
+++ DataSync/MSGraphHelper.php
@@ -1,23 +1,21 @@
 <?php
+use Firebase\JWT\JWT;
+
+//require('JWT.php');
 class MSGraphHelper
 {
-
+    public  $certPath = 'app_only_cert.pfx';
+    public  $certPassword = 'J48W23RQeZv85vj';
 
     public function __construct()
     {
-
-    }
-    public static  function getCert($certPath,$certPassword)
-    {
-        $certPath = 'EduGraphAPI App Only Cert.pfx';
-        $certPassword = 'J48W23RQeZv85vj';
-        $certs = array();
-        $pkcs12 = file_get_contents($certPath );
-        openssl_pkcs12_read( $pkcs12, $certs, $certPassword );
-        return  $certs ;
+//        $this->certPath = getenv("Cert_Path");;
+//        $this->certPassword = getenv("Cert_password");;
     }
 
-    public  static  function  getProvider()
+
+
+    public  function  getProvider()
     {
         $provider = new \League\OAuth2\Client\Provider\GenericProvider([
             'clientId'                => '4e3fa16f-9909-4bf6-9a66-5560e97e7082',    // The client ID assigned to you by the provider
@@ -31,15 +29,51 @@ class MSGraphHelper
 
     }
 
-    public static  function getAccessToken()
+    public   function getAccessToken($tenantId,$clientId)
     {
-        return 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWDhHQ2k2SnM2U0s4MlRzRDJQYjdyeWlvRDBnVjFERHVIZFRpeTl3T1A0UUNEcFVmYXREOW5JMUdCR3kzQ2sxUWE5ZExaWEYxbUdNME9PMk4yRmhHNjdyZV9ral92dkpyWl9KS1pSWWtPSlNBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiaUJqTDFSY3F6aGl5NGZweEl4ZFpxb2hNMllrIiwia2lkIjoiaUJqTDFSY3F6aGl5NGZweEl4ZFpxb2hNMllrIn0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82NDQ0NmI1Yy02ZDg1LTRkMTYtOWZmMi05NGVkZGMwYzI0MzkvIiwiaWF0IjoxNTI1MzM4MjQxLCJuYmYiOjE1MjUzMzgyNDEsImV4cCI6MTUyNTM0MjE0MSwiYWlvIjoiWTJkZ1lKanlJVFhDekUxbkxVLzNKdVBwQWlzZUF3QT0iLCJhcHBfZGlzcGxheW5hbWUiOiJFRFVHcmFwaEFQSSBEZXYgLSBQeXRob24iLCJhcHBpZCI6ImRmYzgxYjk1LTFhOWMtNDUyMi05ZjMzLTI1OWRlOWFjZjY4YiIsImFwcGlkYWNyIjoiMiIsImVfZXhwIjoyNjI4MDAsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzY0NDQ2YjVjLTZkODUtNGQxNi05ZmYyLTk0ZWRkYzBjMjQzOS8iLCJvaWQiOiI0YzcyNj
-A2Mi05ZDJmLTRjMWYtODdiOS1hYWUxNzBiNTk0NjkiLCJyb2xlcyI6WyJVc2VyLlJlYWQuQWxsIl0sInN1YiI6IjRjNzI2MDYyLTlkMmYtNGMxZi04N2I5LWFhZTE3MGI1OTQ2OSIsInR
-pZCI6IjY0NDQ2YjVjLTZkODUtNGQxNi05ZmYyLTk0ZWRkYzBjMjQzOSIsInV0aSI6IjVHN05qUmN3SGtTcVJOYWdiYWdFQUEiLCJ2ZXIiOiIxLjAifQ.mKqAMI6QQn1nZRMZvgVlqmyup-FQCMZG_sWeXcjHfy54JTFWX0t_z9KOtSAMoZ96v7hzwi5KS-GiENcWz0HzHb5tPGgi0cfrugJje4rS1p9ldO0BpLoyR-VIuYV6TIr6jdXsnr8uBOR2tdhKOBh-GklYr4hbw-Ut-s_YqXGAX-0jXB02n3ySNoq3iBbbPJTpvQy0xy9tVQB2-eUtLBSQzAeKlFxm-x3RWtsceMJ9wNr78bbrUXxpyzZIxNkhFpxZ0ytrW3YBkkmypxqnvpqi9qD7pijfTnrBf1TmBGFGhz9uzk0DUSS52b7HpkzatSgPoPquDVs_ZRFI3RoaplaDrg';
+        $jwt = $this->getJWT($tenantId,$clientId);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://login.microsoftonline.com/'.$tenantId.'/oauth2/token?api-version=1.0",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "grant_type=client_credentials&client_id='.$clientId.'&resource=https://graph.microsoft.com&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=".$jwt,
+            CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded"
+            ),
+        ));
+        $response = curl_exec($curl);
+
+        var_dump($response);
+        return $response;
     }
 
-    public function getJWT($key)
+    function getJWT($tenantId,$clientId)
     {
-
+        $payload =
+            ['aud'=> 'https://login.microsoftonline.com/'.$tenantId.'/oauth2/token',
+                'iss'=> $clientId,
+                'sub'=> $clientId,
+                'nbf'=> $_SERVER['REQUEST_TIME'] + 10200,
+                'exp'=> $_SERVER['REQUEST_TIME'] + 20200,
+                'jti'=> 'fe697c1f-5695-463c-a2b8-756065540ce3'
+            ];
+        $cert = $this-> getCert();
+        $key = $cert['pkey'];
+        $jwt = JWT::encode($payload, $key,'RS256',null,['x5t'=> 'u1eozIBvJUJ_0BtXR4wMWdf2JIY=']);
+        return $jwt;
     }
+
+    function getCert()
+    {
+        $certs = array();
+        $pkcs12 = file_get_contents( $this->certPath);
+        openssl_pkcs12_read( $pkcs12, $certs, $this->certPassword );
+        return  $certs ;
+    }
+
 }
--- DataSync/run.php
+++ DataSync/run.php
@@ -1,7 +1,35 @@
+
 <?php
 require_once ('vendor/autoload.php');
 require('MSGraphHelper.php');
 require('DbHelper.php');
+
+$helper = new MSGraphHelper();
+$tenantId = '64446b5c-6d85-4d16-9ff2-94eddc0c2439';
+$clientId = 'dfc81b95-1a9c-4522-9f33-259de9acf68b';
+$jwt = $helper->getAccessToken($tenantId,$clientId);
+//echo( $jwt);
+
+//
+//use \Firebase\JWT\JWT;
+//$tenantId = '64446b5c-6d85-4d16-9ff2-94eddc0c2439';
+//$clientId='dfc81b95-1a9c-4522-9f33-259de9acf68b';
+//$header = ['typ'=> 'JWT', 'alg'=> 'RS256', 'x5t'=> 'u1eozIBvJUJ_0BtXR4wMWdf2JIY='];
+//$payload =
+//    ['aud'=> 'https://login.microsoftonline.com/'.$tenantId.'/oauth2/token',
+//        'iss'=> $clientId,
+//        'sub'=> $clientId,
+//        'nbf'=> $_SERVER['REQUEST_TIME'] + 10200,
+//        'exp'=> $_SERVER['REQUEST_TIME'] + 20200,
+//        'jti'=> 'fe697c1f-5695-463c-a2b8-756065540ce3'
+//    ];
+//
+//
+//$cert = $helper->getCert();
+//$key=$cert['pkey'];
+//
+//$jwt = JWT::encode($payload, $key,'RS256',null,['x5t'=> 'u1eozIBvJUJ_0BtXR4wMWdf2JIY=']);
+//echo $jwt;
 
 //$clientId = 'dfc81b95-1a9c-4522-9f33-259de9acf68b';
 //$token = MSGraphHelper::getAccessToken();

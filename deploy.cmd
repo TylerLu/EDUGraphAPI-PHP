216910419228e631f66ca6e32e8fdbf2433d4f2e
hearticy888 <hearticy@126.com>
2018-05-11 11:04:28 +0800

    Remove unused content.

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,1 @@
-php composer.phar install
 
-:: 1. Deploy WebJobs
-xcopy /y/s "%DEPLOYMENT_SOURCE%\webjobs" "%DEPLOYMENT_TARGET%\App_Data\jobs\triggered\"
-
-:: 2. Create database 
-php artisan make:migration

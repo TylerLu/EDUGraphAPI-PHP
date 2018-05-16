fa0ba1561193a4cc1a2f6816c0efabe8ff14d0a1
hearticy888 <hearticy@126.com>
2018-05-16 16:55:58 +0800

    Update page style.

--- resources/views/link/loginO365Required.blade.php
+++ resources/views/link/loginO365Required.blade.php
@@ -8,7 +8,7 @@
 @extends('layouts.appwithoutlogin')
 @section('title', 'Login to Office 365 is required')
 @section('content')
-    <h2>Login to Office 365 is required</h2>
+    <h2 class="text-danger">Login to Office 365 is required</h2>
     <p>You were redirected to this page for one of the following reasons:</p>
     <p>
     <ul>
@@ -19,7 +19,7 @@
     <p>To continue, please click the button below to login with your Office 365 account.</p>
     <p>
         <a href="{{url('/o365login ')}}"  >
-            <button type="button" class="btn btn-default btn-ms-login" id="OpenIdConnect" name="provider" value="OpenIdConnect" title="Login with Office 365 account">Login with Office 365 account</button>
+            <button type="button" class="btn btn-primary" id="OpenIdConnect" name="provider" value="OpenIdConnect" title="Login with Office 365 account">Login with Office 365 account</button>
         </a>
 
     </p>

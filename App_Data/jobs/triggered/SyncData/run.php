83d638380cda6502e157f174747bf6c37f12580c
Tyler Lu <admin@tyler.lu>
2018-05-30 10:39:21 +0800

    Format code

--- App_Data/jobs/triggered/SyncData/run.php
+++ App_Data/jobs/triggered/SyncData/run.php
@@ -3,13 +3,12 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  *  See LICENSE in the project root for license information.
  */
-require  ('/home/site/wwwroot/vendor/autoload.php');
-require ('bootstrap.php');
+require('/home/site/wwwroot/vendor/autoload.php');
+require('bootstrap.php');
 require('MSGraphHelper.php');
 require('DbHelper.php');
 
 $clientId =getenv("CLIENT_ID");
-
 
 $dbHelper = new DBHelper();
 $msGraphHelper = new MSGraphHelper();
@@ -19,13 +18,9 @@ foreach ($organizations as $org)
 {
      $dataSyncRecord = $dbHelper->getOrCreateDataSyncRecord($org);
      $users = $msGraphHelper->queryUsers($dataSyncRecord->DeltaLink,$org->tenantId,$clientId);
-    foreach ($users as $user) {
-        $dbHelper->updateUser($user);
-    }
+     foreach ($users as $user) {
+          $dbHelper->updateUser($user);
+     }
 }
 
 error_log('Succeed!');
-
-
-
-
